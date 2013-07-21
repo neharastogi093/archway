692aa9f6a80a677e4cd483ec1efb1c81624051d6
Neha <neharastogi093@gmail.com>
2013-07-21 18:44:27 +0530

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archway::Application.config.secret_token = '66323805d361559a306ef3c09647441f4a72835bebaecfe684daf167e9ee8cb1e1fb11e911d01a076750a635fa6c093b2ace6494d570c2c86962ec564fd9e689'
